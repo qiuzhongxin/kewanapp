1dd84b123991a34fa0f0be62d0a75ab6a3913d84
qiuzhongxin <397260568@qq.com>
2024-12-14 21:30:44 +0800

    add kewanicloud

---
+++ kewanicloud/kewanicloud/kewanicloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  kewanicloudApp.swift
+//  kewanicloud
+//
+//  Created by Zhongxin qiu on 2024/12/14.
+//
+
+import SwiftUI
+
+@main
+struct kewanicloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
